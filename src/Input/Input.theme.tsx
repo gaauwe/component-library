b449f13c24a461edd4f8966cb530c70328719cec
Gaauwe Rombouts <gromdroid@gmail.com>
2022-01-24 22:21:30 +0100

    Refactored Link to new folder structure

---
+++ src/Input/Input.theme.tsx
@@ -0,0 +1,3 @@
+export type inputThemeType = {
+  theme: string;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,5 @@
 export { default as Button } from "./Button/Button";
 export * from "./Button/Button";
+
+export { default as Link } from "./Link/Link";
+export * from "./Link/Link";
